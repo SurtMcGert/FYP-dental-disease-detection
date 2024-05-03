a27da307681800a073d34c96d18e8e91d80a364d
SurtMcGert <harry.ayre@outlook.com>
2024-05-03 12:56:38 +0100

    trained and evaluated self attention layer

--- README.md
+++ README.md
@@ -5,44 +5,14 @@
 The dataset consists of 705 radiographs with 5 stages of dental caries provided by DENTEX
 Healthy, Caries, Deep Caries, Periapical Lesion, Impacted Tooth
 
-## semi-enhanced Branch
+## self-attention Branch
 
 This branch contains the implementation of Self Attention on the YOLOv6 Model
 
-### 500 epochs YOLOv6s
+### 500 epochs YOLOv6s + Self Attention
 
-10.8 hours
-
-| Class | Images | Labels | P@.5iou | R@.5iou | F1@.5iou | mAP@.5 | mAP@.5:.95 |
-| ----- | ------ | ------ | ------- | ------- | -------- | ------ | ---------- |
-| all   | 141    | 732    | 0.925   | 0.771   | 0.839    | 0.867  | 0.629      |
-
-### 500 epochs YOLOv6s6
-
-9.6 hours
+13.27 hours
 
 | Class | Images | Labels | P@.5iou | R@.5iou | F1@.5iou | mAP@.5 | mAP@.5:.95 |
 | ----- | ------ | ------ | ------- | ------- | -------- | ------ | ---------- |
-| all   | 141    | 732    | 0.914   | 0.824   | 0.866    | 0.873  | 0.635      |
-
-### 500 epochs YOLOv6s6 SGD Optimized
-
-0.13 hours to make initial weights 11.62 hours to get hyperparams, 12.10 hours to train
-
-best mAP: 0.11704626679420471
-the best hyperparameters: tensor([1.0000e-01, 1.0000e-03, 8.8008e-01, 1.0000e-04])
-
-| Class | Images | Labels | P@.5iou | R@.5iou | F1@.5iou | mAP@.5 | mAP@.5:.95 |
-| ----- | ------ | ------ | ------- | ------- | -------- | ------ | ---------- |
-| all   | 141    | 732    | 0.924   | 0.805   | 0.859    | 0.857  | 0.648      |
-
-### 500 epochs YOLOv6s6 Adam Optimized
-
-0.13 hours to make initial weights 12.00 hours to get hyperparams, 15.14 hours to train
-
-best mAP: 0.3040043115615845
-the best hyperparameters: tensor([4.7758e-02, 1.0000e-03, 9.0000e-01, 1.0000e-04])
-
-| Class | Images | Labels | P@.5iou | R@.5iou | F1@.5iou | mAP@.5 | mAP@.5:.95 |
-| ----- | ------ | ------ | ------- | ------- | -------- | ------ | ---------- |
-| all   | 141    | 732    | 0.774   | 0.698   | 0.732    | 0.755  | 0.5        |
+| all   | 141    | 732    | 0.874   | 0.83    | 0.851    | 0.866  | 0.627      |
--- yolov6/models/efficientrep.py
+++ yolov6/models/efficientrep.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch
 from yolov6.layers.common import BottleRep, RepVGGBlock, RepBlock, BepC3, SimSPPF, SPPF, SimCSPSPPF, CSPSPPF, ConvBNSiLU, \
     MBLABlock, ConvBNHS, Lite_EffiBlockS2, Lite_EffiBlockS1
+import math
 
 
 class EfficientRep(nn.Module):
@@ -229,20 +230,9 @@ class EfficientRep6(nn.Module):
             )
         )
 
-        # self attention layers
-        self.conv_q = nn.Conv2d(
-            channels_list[5], channels_list[5], kernel_size=3, padding=1)
-        self.conv_k = nn.Conv2d(
-            channels_list[5], channels_list[5], kernel_size=3, padding=1)
-        self.conv_v = nn.Conv2d(
-            channels_list[5], channels_list[5], kernel_size=3, padding=1)
-
-    def self_attention(self, x):
-        # q = self.conv_q(x)
-        # k = self.conv_k(x)
-        # v = self.conv_v(x)
-        # attention_scores = torch.einsum("bnqd,bnkd->bnqk", q, k)
-        print(x.cpu().size())
+        self.num_of_attention_heads = 4
+        self.attention_layer = MultiHeadedSelfAttention(
+            channels_list[5], int(channels_list[5]/self.num_of_attention_heads), int(channels_list[5]/self.num_of_attention_heads), int(channels_list[5]/self.num_of_attention_heads), self.num_of_attention_heads)
 
     def forward(self, x):
 
@@ -258,11 +248,93 @@ class EfficientRep6(nn.Module):
         x = self.ERBlock_5(x)
         outputs.append(x)
         x = self.ERBlock_6(x)
-        attention = self.self_attention(x)
+        x = self.attention_layer(x)
         outputs.append(x)
-        print(x.size())
 
         return tuple(outputs)
+
+
+class MultiHeadedSelfAttention(nn.Module):
+    def __init__(self, input_dim, q_dim, k_dim, v_dim, num_heads):
+        assert input_dim % num_heads == 0, f"input_dim ({input_dim}) must be divisible by num_heads ({num_heads})"
+        assert isinstance(
+            input_dim, int), f"input_dim ({input_dim}) must be an integer"
+        assert isinstance(q_dim, int), f"q_dim ({q_dim}) must be an integer"
+        assert isinstance(k_dim, int), f"k_dim ({k_dim}) must be an integer"
+        assert isinstance(v_dim, int), f"v_dim ({v_dim}) must be an integer"
+        super(MultiHeadedSelfAttention, self).__init__()
+        self.input_dim = input_dim  # List of input/output channel dimensions
+        self.q_dim = q_dim  # Dimensionality per head for queries
+        self.k_dim = k_dim  # Dimensionality per head for keys
+        self.v_dim = v_dim  # Dimensionality per head for values
+        self.num_of_heads = num_heads  # Number of attention heads
+
+        # print(f"input_dim: {self.input_dim}")
+        # print(f"q_dim: {self.q_dim}")
+        # print(f"k_dim: {self.k_dim}")
+        # print(f"v_dim: {self.v_dim}")
+        # print(f"num_of_heads: {self.num_of_heads}")
+
+        # Self-attention layers (Conv2d with kernel_size=1)
+        self.conv_q = nn.Conv2d(
+            self.input_dim, self.q_dim * self.num_of_heads, kernel_size=1)
+        self.conv_k = nn.Conv2d(
+            self.input_dim, self.k_dim * self.num_of_heads, kernel_size=1)
+        self.conv_v = nn.Conv2d(
+            self.input_dim, self.v_dim * self.num_of_heads, kernel_size=1)
+
+        # Weight for final linear projection (output)
+        # self.fc = nn.Linear(self.num_heads * self.v_dim,
+        #                     self.channels_list[6], bias=False)
+
+    def forward(self, x):
+        batch_size, num_channels, height, width = x.size()
+
+        # Project using Conv2d layers
+        q = self.conv_q(x)
+        k = self.conv_k(x)
+        v = self.conv_v(x)
+
+        # print(f"q: {q.size()}")
+        # print(f"k: {k.size()}")
+        # print(f"v: {v.size()}")
+
+        # Reshape for multi-headed attention
+        q_heads = q.view(batch_size, self.num_of_heads,
+                         self.q_dim, height, width)
+        k_heads = k.view(batch_size, self.num_of_heads,
+                         self.k_dim, height, width)
+        v_heads = v.view(batch_size, self.num_of_heads,
+                         self.v_dim, height, width)
+
+        # print(f"q_heads: {q_heads.size()}")
+        # print(f"k_heads: {k_heads.size()}")
+        # print(f"v_heads: {v_heads.size()}")
+
+        # calculate the attention scores
+        attention_scores = torch.matmul(
+            q_heads, k_heads.transpose(-2, -1)) / math.sqrt(self.k_dim)
+
+        # print(f"attention_scores: {attention_scores.size()}")
+
+        # calculate the attention weights
+        attention_weights = torch.nn.functional.softmax(
+            attention_scores, dim=-1)
+
+        # print(f"attention_weights: {attention_weights.size()}")
+
+        # calculate the new contexts
+        new_contexts = torch.matmul(attention_weights, v_heads)
+
+        # print(f"new_contexts: {new_contexts.size()}")
+
+        # Concatenate heads and apply final linear projection
+        output = new_contexts.view(
+            batch_size, -1, height, width)  # Reshape before concatenation
+
+        # print(f"output: {output.size()}")
+
+        return output
 
 
 class CSPBepBackbone(nn.Module):
